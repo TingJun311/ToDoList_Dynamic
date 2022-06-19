6c635de799a926656000d0886d03dc2f8c640dfb
Isaac <swengineerwithisaac@gmail.com>
2022-06-19 22:05:06 +0800

    Deploy to heroku

--- app.js
+++ app.js
@@ -152,8 +152,10 @@ app.get("/:category", (req, res) => {
     });
 });
 
-
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "")
+    port = 3000;
+app.listen(port, function () {
     // A localhost:3000
     console.log("Server started on port 3000");
 });
